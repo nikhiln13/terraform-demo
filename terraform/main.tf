326bbc05e3660eebc87040068d48807be2e9b1a6
Nikhil Nalage <88099899+nikhiln13@users.noreply.github.com>
2025-12-31 13:25:59 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    amazon-linux-extras install docker -y
+    systemctl start docker
+    systemctl enable docker
+    docker run -d -p 80:80 ${var.image_url}
+  EOF
+
+  tags = {
+    Name = var.instance_name
+  }
+}
